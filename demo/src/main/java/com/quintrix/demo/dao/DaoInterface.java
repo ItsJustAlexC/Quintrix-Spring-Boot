8a9fecc8fe6806472989aee618e777d0cefb44e9
ItsJustAlexC <40281029+ItsJustAlexC@users.noreply.github.com>
2022-09-07 18:47:41 -0400

    demo update

---
+++ demo/src/main/java/com/quintrix/demo/dao/DaoInterface.java
@@ -0,0 +1,24 @@
+package com.quintrix.demo.dao;
+
+import java.util.List;
+
+import com.quintrix.demo.model.User;
+
+public interface DaoInterface {
+
+	int createUser(int id, String firstName, String lastName);
+
+	int createUser(User user);
+
+	int updateUser(int id, String firstName, String lastName);
+
+	int updateUser(User user);
+
+	User selectUser(int id);
+
+	List<User> selectAll();
+
+	int deleteUser(int id);
+
+	void deleteAll();
+}
